e19f2f60e4b46bf4ddc7580cbf23930991017426
Vladimir <kremench@gmail.com>
2025-02-14 11:20:54 +0200

    start parameter and file

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,13 @@
+// var API_KEY = '48839660-7b8b283c3689698998fc631e5';
+// var URL =
+//   'https://pixabay.com/api/?key=' +
+//   API_KEY +
+//   '&q=' +
+//   encodeURIComponent('red roses');
+// $.getJSON(URL, function (data) {
+//   if (parseInt(data.totalHits) > 0)
+//     $.each(data.hits, function (i, hit) {
+//       console.log(hit.pageURL);
+//     });
+//   else console.log('No hits');
+// });
